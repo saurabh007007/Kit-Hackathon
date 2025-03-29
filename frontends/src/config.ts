913394b148c291d9f67f88c3960d7518b4f180a2
saurabh007007 <loverboybhol143@gamil.com>
2025-03-29 06:28:09 +0530

    final touch

--- frontends/src/config.ts
+++ frontends/src/config.ts
@@ -1,2 +1,3 @@
 // export const BACKEND_URL = "http://api.duo.sya007.live";
-export const BACKEND_URL = "http://localhost:3000";
+// export const BACKEND_URL = "http://localhost:3000";
+export const BACKEND_URL = "http://api.kit25.app-x.live";
